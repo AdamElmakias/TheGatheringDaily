09f62d613f97a1d1b14a586e92ed56295df57e64
Adam Elmakias <adamelmakias@adams-mbp.attlocal.net>
2018-10-26 16:15:43 -0700

    dan added

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import Header from "../components/header"
 export default () => (
   <div style={{ color: `purple` }}>
     <Link to="/contact/">Contact</Link>
-    <Header headerText="DOM IS MY FRIEND!" />
+    <Header headerText="DOM IS MY FRIEND! Dan is my friend too, one day ill have a tesla" />
     <p>What a world.</p>
     <img src="http://www.adamelmakias.com/wp-content/uploads/2018/10/TheGatheringDailyLogo.png" alt="" />
   </div>
