2c9db2eaa2425ae23da8f71732b88b208da80620
Adam Elmakias <adamelmakias@adams-mbp.attlocal.net>
2018-10-25 23:42:58 -0700

    Inital commit

---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Header from "../components/header"
+
+export default () => (
+  <div style={{ color: `teal` }}>
+    <Header headerText="About Gatsby" />
+    <Header headerText="I HATE FISH" />
+    <Header headerText="FUCK YOUR SEA FOOD" />
+    <p>Such wow. Very React.</p>
+  </div>
+)
---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { Link } from "gatsby"
+import Header from "../components/header"
+
+export default () => (
+  <div style={{ color: `purple` }}>
+    <Link to="/contact/">Contact</Link>
+    <Header headerText="The Gathering Daily!" />
+    <p>What a world.</p>
+    <img src="http://www.adamelmakias.com/wp-content/uploads/2018/10/TheGatheringDailyLogo.png" alt="" />
+  </div>
+)
