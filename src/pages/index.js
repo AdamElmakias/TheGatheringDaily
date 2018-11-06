5c55d113bec2af3c2409d4499a0058dd08f0ab69
Adam Elmakias <adamelmakias@adams-mbp.attlocal.net>
2018-11-06 15:40:40 -0800

    ta da

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,38 +2,9 @@ import React from "react"
 import { Link, graphql } from "gatsby"
 import Header from "../components/header"
 import styled from 'styled-components'
-
-export const query = graphql`
-query getArticle {
-  allContentfulArticlePost {
-    edges {
-      node {
-        title        
-      }
-    }
-  }
-}
-`
-
 export default ({data}) => {
   const articles = data.allContentfulArticlePost.edges
   console.log(articles)
-
-  return (
-    <div style={{ color: `purple` }}>
-      <Link to="/contact/">Contact</Link>
-      <Header headerText="DOM IS MY FRIEND! Dan is my friend too, one day ill have a tesla" />
-      <p>What a world.</p>
-      <img src="http://www.adamelmakias.com/wp-content/uploads/2018/10/TheGatheringDailyLogo.png" alt="" />
-
-      {articles.map((article, index) => {
-        return(
-          <Article key={index} article={article} />
-        )
-      })}
-    </div>
-  )
-}
 
 const Article = ({article}) => {
   return (
